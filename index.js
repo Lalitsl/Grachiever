ab3d099933a669d43acdc1d2c21835f2359a1537
Lalit Narware <lalitnarware2001@gmail.com>
2023-04-13 01:06:33 +0530

    z-index new section added please check it

--- index.js
+++ index.js
@@ -56,3 +56,35 @@ function submitotp(){
     document.getElementById("otpdiv").style.display="block";
 }
 // ................... forgot password js is end here .......................
+
+
+// ..................... card section z -index start ..............
+document.getElementById('scard01').addEventListener("mouseover", myfunction);
+function myfunction()
+ {
+    document.getElementById('scard01').style.backgroundColor = "red";
+}
+
+
+function sd01(){
+    document.getElementById('scard01').style.backgroundColor = "green";
+    document.getElementById('scard01').style.zIndex =0;
+    document.getElementById('scard01').style.marginTop ="-9px";
+    document.getElementById("scard01").style.transitionDuration="3s";
+
+}
+function sd02(){
+    document.getElementById('scard02').style.backgroundColor = "red";
+    document.getElementById('scard02').style.zIndex =30;
+    document.getElementById('scard02').style.marginTop ="-9px";
+    document.getElementById("scard02").style.transitionDuration="3s";
+}
+function sd03(){
+    document.getElementById('scard03').style.backgroundColor = "orange";
+    document.getElementById('scard03').style.zIndex =40;
+    document.getElementById('scard03').style.marginTop ="-9px";
+    document.getElementById("scard03").style.transitionDuration="3s";
+}
+
+
+// ..................... card section z -index start ..............
