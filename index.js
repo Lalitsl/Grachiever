633c959e0585636ed71b9aab642ce47cd64f8cb1
Lalit Narware <lalitnarware2001@gmail.com>
2023-04-14 02:43:09 +0530

    again home page design change

--- index.js
+++ index.js
@@ -59,32 +59,51 @@ function submitotp(){
 
 
 // ..................... card section z -index start ..............
-document.getElementById('scard01').addEventListener("mouseover", myfunction);
-function myfunction()
- {
-    document.getElementById('scard01').style.backgroundColor = "red";
-}
+
+// document.getElementById('scard01').addEventListener("click",()=>{
+//     var id1=this.id;
+//     console.log("this is id");
+// })
+
+// document.getElementById('scard01').addEventListener("mouseover", myfunction);
+// function myfunction()
+//  {
+//     document.getElementById('scard01').style.backgroundColor = "red";
+// }
 
 
-function sd01(){
-    document.getElementById('scard01').style.backgroundColor = "green";
-    document.getElementById('scard01').style.zIndex =0;
-    document.getElementById('scard01').style.marginTop ="-9px";
-    document.getElementById("scard01").style.transitionDuration="3s";
-
-}
-function sd02(){
-    document.getElementById('scard02').style.backgroundColor = "red";
-    document.getElementById('scard02').style.zIndex =30;
-    document.getElementById('scard02').style.marginTop ="-9px";
-    document.getElementById("scard02").style.transitionDuration="3s";
-}
-function sd03(){
-    document.getElementById('scard03').style.backgroundColor = "orange";
-    document.getElementById('scard03').style.zIndex =40;
-    document.getElementById('scard03').style.marginTop ="-9px";
-    document.getElementById("scard03").style.transitionDuration="3s";
-}
 
 
-// ..................... card section z -index start ..............
+document.getElementById("scard01").addEventListener("click", ()=>{
+    document.querySelector("#scard02").classList.remove("lift-up");
+    document.querySelector("#scard03").classList.remove("lift-up");
+    document.querySelector("#scard01").classList.add("shift-left");
+    setTimeout(() => {
+        document.querySelector("#scard01").classList.toggle("lift-up");
+    }, 500);
+    setTimeout(() => {
+        document.querySelector("#scard01").classList.remove("shift-left");
+    }, 700);
+})
+document.getElementById("scard02").addEventListener("click", ()=>{
+    document.querySelector("#scard01").classList.remove("lift-up");
+    document.querySelector("#scard03").classList.remove("lift-up");
+    document.querySelector("#scard02").classList.add("shift-left");
+    setTimeout(() => {
+        document.querySelector("#scard02").classList.toggle("lift-up");
+    }, 2000);
+    setTimeout(() => {
+        document.querySelector("#scard02").classList.remove("shift-left");
+    }, 700);
+})
+document.getElementById("scard03").addEventListener("click", ()=>{
+    document.querySelector("#scard02").classList.remove("lift-up");
+    document.querySelector("#scard01").classList.remove("lift-up");
+    document.querySelector("#scard03").classList.add("shift-right");
+    setTimeout(() => {
+        document.querySelector("#scard03").classList.toggle("lift-up");
+    }, 500);
+    setTimeout(() => {
+        document.querySelector("#scard03").classList.remove("shift-right");
+    }, 700);
+})
